a5d0d3dc64369103c82755bd00f5f1b636a12014
Maximiliano Arias <ariasmaxi89@gmail.com>
2019-08-01 10:36:47 -0300

    updatedv2

--- example/app.js
+++ example/app.js
@@ -1,15 +1,26 @@
 const SV = require('../lib/segview');
 const config = require('./config');
-const util = require('util')
 
 const sv = new SV(config.apiKey, config.secret);
+const pictures = [
+  {
+    name: 'picture_0',
+    link: 'https://www.carsdrive.com.ar/wp-content/uploads/2015/07/FullSizeRenderfgf_resize.jpg',
+  },
+  {
+    name: 'picture_1',
+    link: 'https://www.carsdrive.com.ar/wp-content/uploads/2015/07/FullSizeRenderfgf_resize.jpg',
+  },
+];
 sv.sendPhoto(
-  './testfile.jpg',
-  'ab123cd',
+  pictures,
+  true,
   (res, err) => {
     if (err) throw err;
-    console.log(util.inspect(res, {showHidden: false, depth: null}))
+    // Getting the posted certification back
+    if (res) {
+      console.log('Getting back the certification ');
+      console.log(res.response.certificationId);
+    }
   },
 );
-
-
